82dce9fe784be8ef56ce1f6e44a5e24dbb195ae9
Richard White <richard.white@qmul.ac.uk>
2024-04-23 18:48:14 +0100

    Add running headloop.design
    Add basic functionality to take the form input and run headloop
    design and then return the results
    
    New template: output.html

--- app/forms.py
+++ app/forms.py
@@ -3,13 +3,16 @@ from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class InputForm(FlaskForm):
-    primer_f = StringField('Forward Primer', validators=[DataRequired()])
-    primer_r = StringField('Reverse Primer', validators=[DataRequired()])
+    primer_f = StringField('Forward Primer', validators=[DataRequired()],
+    default = "CTGGTCCAGTGCGTTATTGG")
+    primer_r = StringField('Reverse Primer', validators=[DataRequired()],
+    default = "AGCCAAATGCTTCTTGCTCTTTT")
     guide_seq = StringField('Guide Sequence and context (>=15 bp)',
         validators=[
             DataRequired(),
             Length(min = 38, message="The gRNA target sequence (including PAM) and at least 15 bp afterwards must be provided")
-        ])
+        ],
+        default = "CTACAGGACGTACCTGCACCCGGATTCACCAGCGCCCG")
     orientation = SelectField('Orientation', 
-        choices = ["Forward", "Reverse"], validators=[DataRequired()])
+        choices = [("sense", "Forward"), ("antisense", "Reverse")], validators=[DataRequired()])
     submit = SubmitField('Design Headloop Primers')
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,35 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, url_for
 from app import app
 from app.forms import InputForm
+
+import os
+from headloop.designer import design
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     form = InputForm()
     if form.validate_on_submit():
-        flash('Form submitted F={}, R={}, G={}, O={}'.format(
-            form.primer_f.data, form.primer_r.data,
-            form.guide_seq.data, form.orientation.data))
-        return redirect('/index')
+        if os.getenv("FLASK_DEBUG"):
+            flash('Form submitted F={}, R={}, G={}, O={}'.format(
+                form.primer_f.data, form.primer_r.data,
+                form.guide_seq.data, form.orientation.data))
+        hl_design = design(form.primer_f.data, form.primer_r.data,
+            form.guide_seq.data, form.orientation.data)
+        hl_results = []
+        for result in hl_design:
+            if result.description[0:8] == "WARNING:":
+                desc = result.description[9:]
+                desc_class = "warning"
+            else:
+                desc = result.description
+                desc_class = "success"
+            hl_results.append((result.id, str(result.seq),
+                desc, desc_class))
+        return render_template(
+            'output.html',
+            primer_f = form.primer_f.data,
+            primer_r = form.primer_r.data,
+            hl_results = hl_results
+        )
     return render_template('index.html', form=form)
