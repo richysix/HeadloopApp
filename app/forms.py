66570c9ea14422b98ee7dd13889770d328c7c905
Richard White <richard.white@qmul.ac.uk>
2024-04-23 14:41:13 +0100

    Change Orientation in InputForm to Select Field

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 
 class InputForm(FlaskForm):
     primer_f = StringField('Forward Primer', validators=[DataRequired()])
     primer_r = StringField('Reverse Primer', validators=[DataRequired()])
     guide_seq = StringField('Guide Sequence and context (>=15 bp)', validators=[DataRequired()])
-    orientation = StringField('Orientation', validators=[DataRequired()])
+    orientation = SelectField('Orientation', 
+        choices = ["Forward", "Reverse"], validators=[DataRequired()])
     submit = SubmitField('Design Headloop Primers')
