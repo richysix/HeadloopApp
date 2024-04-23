af1e12818e41db61aef5907b7f59df64e805116f
Richard White <richard.white@qmul.ac.uk>
2024-04-23 17:32:41 +0100

    Add length validation to guide_seq field

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 class InputForm(FlaskForm):
     primer_f = StringField('Forward Primer', validators=[DataRequired()])
     primer_r = StringField('Reverse Primer', validators=[DataRequired()])
-    guide_seq = StringField('Guide Sequence and context (>=15 bp)', validators=[DataRequired()])
+    guide_seq = StringField('Guide Sequence and context (>=15 bp)',
+        validators=[
+            DataRequired(),
+            Length(min = 38, message="The gRNA target sequence (including PAM) and at least 15 bp afterwards must be provided")
+        ])
     orientation = SelectField('Orientation', 
         choices = ["Forward", "Reverse"], validators=[DataRequired()])
     submit = SubmitField('Design Headloop Primers')
