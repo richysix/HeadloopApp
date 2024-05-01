cc4252d73f7c6e3030a3e2755320f7e17f49e545
Richard White <richard.white@qmul.ac.uk>
2024-05-01 16:01:29 +0100

    Add test_config argument to create_app

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from config import Config
 
-def create_app(config_class=Config):
+def create_app(config_class=Config, test_config=None):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    if test_config is None:
+        app.config.from_object(Config)
+    else:
+        app.config.from_mapping(test_config)
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
