553b3c8b49b8a98352c4a494cad4b11f41d2c39e
Richard White <richard.white@qmul.ac.uk>
2024-05-01 16:02:34 +0100

    Move errorhandler to main create_app function
    A 404 errorhandler does not work in a Blueprint
    Needs to be registered with the main app

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 from config import Config
+
+def not_found_error(error):
+    return render_template('404.html'), 404
 
 def create_app(config_class=Config, test_config=None):
     app = Flask(__name__)
@@ -10,5 +13,7 @@ def create_app(config_class=Config, test_config=None):
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
+
+    app.register_error_handler(404, not_found_error)
 
     return app
--- app/main/routes.py
+++ app/main/routes.py
@@ -72,7 +72,3 @@ def download():
     resp.headers['Content-Disposition'] = 'filename="hl.csv"'
     resp.headers['Content-type'] = 'text/csv'
     return resp
-
-@bp.errorhandler(404)
-def not_found_error(error):
-    return render_template('404.html'), 404
