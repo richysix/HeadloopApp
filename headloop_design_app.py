43857baaf4c8dad731dfc91be0f7215545fb5164
Richard White <richard.white@qmul.ac.uk>
2024-05-01 17:11:23 +0100

    Test middleware to remove fcgi from URL

--- headloop_design_app.py
+++ headloop_design_app.py
@@ -1,4 +1,11 @@
 from app import create_app
 
-app = create_app()
+class ScriptNameStripper(object):
+   def __init__(self, app):
+       self.app = app
 
+   def __call__(self, environ, start_response):
+       environ['SCRIPT_NAME'] = ''
+       return self.app(environ, start_response)
+
+app = ScriptNameStripper(create_app())
